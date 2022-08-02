94e91ee2f0e3b3d9db73f09cce585079b04e6fce
Josenunez20 <93611190+Josenunez20@users.noreply.github.com>
2022-08-02 14:33:31 -0400

    Update and rename src/components/pages/Home.jsx to origen/componentes/paginas/Home.jsx

--- origen/componentes/paginas/Home.jsx
+++ origen/componentes/paginas/Home.jsx
@@ -2,6 +2,10 @@ import Banner from "/banner_img.png";
 import Clicks from '/student_click.png';
 import Homestay from '/homestay.png';
 import Save from '/save.png';
+import Accommodation from '/accommodation_home.png';
+import Homestayhome from '/homestay_home.png';
+import Student from '/student_home.png';
+import Download from '/download.png';
 
 const Banner_titulo = 'Simplify Your Homestay process';
 const Banner_contenido = 
@@ -16,7 +20,17 @@ const Homestay_texto = 'Homestay families easily set up their availability and c
 
 const Save_titulo = 'Save time and money';
 const Save_texto = 'Get a real time availability of your homestay families without making a single phone call. Once the booking is confirmed everyone gets notified automatically.';
+const Service_titulo = 'Accommodation Provider';
+const Service_texto = 'Find the perfect match, availability and book a room in just a few clicks.';
 
+const Service_titulo_2 = 'Homestay';
+const Service_texto_2 = 'One simple and intuitive app to update availability, confirm bookings and communicate with the school.';
+
+const Service_titulo_3 = 'Student';
+const Service_texto_3 = 'Students take active part of the matching process, significantly reducing changes and cancellations.';
+
+const Download_titulo = 'Now Available!';
+const Download_texto = 'We are working to give you a better experience from your mobile device';
 const Home = () => {
   return (
     <>
@@ -58,6 +72,48 @@ const Home = () => {
           <p>{Save_texto}</p>
         </div>
       </div>
+             <div className="container__service">
+        <div className="card">  
+          <div className="img_accommodation">
+            <img src={Accommodation} alt="Imagen Accommodation"/>
+          </div>
+          <div className="card-body">
+            <h1>{Service_titulo}</h1>
+            <p>{Service_texto}</p>
+          </div>
+        </div>
+      
+        <div className="card">  
+          <div className="img_homestayhome">
+            <img src={Homestayhome} alt="Imagen Homestayhome"/>
+          </div>
+          <div className="card-body">
+            <h1>{Service_titulo_2}</h1>
+            <p>{Service_texto_2}</p>
+          </div>
+        </div>
+      
+        <div className="card">
+          <div className="img_student">
+            <img src={Student} alt="Imagen Student"/>
+          </div>
+          <div className="card-body">
+            <h1>{Service_titulo_3}</h1>
+            <p>{Service_texto_3}</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="container__download">
+        <div className="container__texto--download">
+          <h1>{Download_titulo}</h1>
+          <p>{Download_texto}</p>
+        </div>
+        <div className="img_download">
+          <img src={Download} alt="Imagen download" />
+        </div>
+      </div>
+
     </>
   );
 };
