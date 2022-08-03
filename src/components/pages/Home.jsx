08c85a30d16dc1302311891447c7d674b7166214
Josenunez20 <joseurpino@gmail.com>
2022-08-03 15:25:45 -0400

    Update Home.jsx

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,39 +1,50 @@
 import Banner from "/banner_img.png";
-import Clicks from '/student_click.png';
-import Homestay from '/homestay.png';
-import Save from '/save.png';
-import Accommodation from '/accommodation_home.png';
-import Homestayhome from '/homestay_home.png';
-import Student from '/student_home.png';
-import Download from '/download.png';
-import Accommodationhome from '/accommodation_home1.png';
+import Clicks from "/student_click.png";
+import Homestay from "/homestay.png";
+import Save from "/save.png";
+import Accommodation from "/accommodation_home.png";
+import Homestayhome from "/homestay_home.png";
+import Student from "/student_home.png";
+import Download from "/download.png";
+import Accommodationhome from "/accommodation_home1.png";
+import { Link } from "react-router-dom";
 
-const Banner_titulo = 'Simplify Your Homestay process';
-const Banner_contenido = 
-    'Homebor is a student accommodation platform that efficiently handles your entire business process, maximizing the experience for your team, your homestay families and your students';
-const Button = <h3 className="texto_button">Book a Free Demo</h3>
+const Banner_titulo = "Simplify Your Homestay process";
+const Banner_contenido =
+  "Homebor is a student accommodation platform that efficiently handles your entire business process, maximizing the experience for your team, your homestay families and your students";
+const Button = <h3 className="texto_button">Book a Free Demo</h3>;
 
-const Clicks_titulo = 'Perfect Matchmaking with just a few clicks';
-const Clicks_texto = 'Our platform manages the selection process in a simple and intuitive way, integrating everyone.';
+const Clicks_titulo = "Perfect Matchmaking with just a few clicks";
+const Clicks_texto =
+  "Our platform manages the selection process in a simple and intuitive way, integrating everyone.";
 
-const Homestay_titulo = 'Happy Families, Happy Students';
-const Homestay_texto = 'Homestay families easily set up their availability and confirm their bookings in our intuitive app.';
+const Homestay_titulo = "Happy Families, Happy Students";
+const Homestay_texto =
+  "Homestay families easily set up their availability and confirm their bookings in our intuitive app.";
 
-const Save_titulo = 'Save time and money';
-const Save_texto = 'Get a real time availability of your homestay families without making a single phone call. Once the booking is confirmed everyone gets notified automatically.';
+const Save_titulo = "Save time and money";
+const Save_texto =
+  "Get a real time availability of your homestay families without making a single phone call. Once the booking is confirmed everyone gets notified automatically.";
 
-const Service_titulo = 'Accommodation Provider';
-const Service_texto = 'Find the perfect match, availability and book a room in just a few clicks.';
+const Service_titulo = "Accommodation Provider";
+const Service_texto =
+  "Find the perfect match, availability and book a room in just a few clicks.";
 
-const Service_titulo_2 = 'Homestay';
-const Service_texto_2 = 'One simple and intuitive app to update availability, confirm bookings and communicate with the school.';
+const Service_titulo_2 = "Homestay";
+const Service_texto_2 =
+  "One simple and intuitive app to update availability, confirm bookings and communicate with the school.";
 
-const Service_titulo_3 = 'Student';
-const Service_texto_3 = 'Students take active part of the matching process, significantly reducing changes and cancellations.';
+const Service_titulo_3 = "Student";
+const Service_texto_3 =
+  "Students take active part of the matching process, significantly reducing changes and cancellations.";
 
-const Download_titulo = 'Now Available!';
-const Download_texto = 'We are working to give you a better experience from your mobile device';
+const Download_titulo = "Now Available!";
+const Download_texto =
+  "We are working to give you a better experience from your mobile device";
 
+const Accommodationhome_titulo = "Work With Us";
+const Accommodationhome_texto =
+  "We are reaching out to a small number of schools to offer our platform, at no cost, in exchange of only their feedback and ideas. It is extremely important for us to continue to build a product that add value to schools and family and creates the best matchmaking for students.";
 const Home = () => {
   return (
     <>
@@ -41,18 +52,16 @@ const Home = () => {
       <div className="container__banner">
         <h1 className="titulo_banner">{Banner_titulo}</h1>
         <p className="texto_banner">{Banner_contenido}</p>
-        <button className="button__banner">
-            {Button}
-        </button>
+        <button className="button__banner">{Button}</button>
       </div>
 
       <div className="clicks">
         <div className="img_clicks">
-            <img src={Clicks} alt="Imagen Click" />
+          <img src={Clicks} alt="Imagen Click" />
         </div>
         <div className="container__texto--clicks">
-            <h1>{Clicks_titulo}</h1>
-            <p>{Clicks_texto}</p>
+          <h1>{Clicks_titulo}</h1>
+          <p>{Clicks_texto}</p>
         </div>
       </div>
 
@@ -75,31 +84,31 @@ const Home = () => {
           <p>{Save_texto}</p>
         </div>
       </div>
-
+      
       <div className="container__service">
-        <div className="card">  
+        <div className="card">
           <div className="img_accommodation">
-            <img src={Accommodation} alt="Imagen Accommodation"/>
+            <img src={Accommodation} alt="Imagen Accommodation" />
           </div>
           <div className="card-body">
             <h1>{Service_titulo}</h1>
             <p>{Service_texto}</p>
           </div>
         </div>
-      
-        <div className="card">  
+
+        <div className="card">
           <div className="img_homestayhome">
-            <img src={Homestayhome} alt="Imagen Homestayhome"/>
+            <img src={Homestayhome} alt="Imagen Homestayhome" />
           </div>
           <div className="card-body">
             <h1>{Service_titulo_2}</h1>
             <p>{Service_texto_2}</p>
           </div>
         </div>
-      
+
         <div className="card">
           <div className="img_student">
-            <img src={Student} alt="Imagen Student"/>
+            <img src={Student} alt="Imagen Student" />
           </div>
           <div className="card-body">
             <h1>{Service_titulo_3}</h1>
@@ -118,14 +127,15 @@ const Home = () => {
         </div>
       </div>
 
-      <div className="container__save">
-        <div className="img_save">
-          <img src={Save} alt="Imagen Save" />
+      <div className="container__accommodationhome">
+        <div className="img_accommodationhome">
+          <img src={Accommodationhome} alt="Imagen Accommodationhome" />
         </div>
-        <div className="container__texto--save">
-          <h1>{Save_titulo}</h1>
-          <p>{Save_texto}</p>
+        <div className="container__texto--accommodationhome">
+          <h1>{Accommodationhome_titulo}</h1>
+          <p>{Accommodationhome_texto}</p>
         </div>
+        <a className="button__accommodationhome" href="https://calendly.com/homebor/demo?month=2021-08"> Book a Free Demo</a>
       </div>
     </>
   );
