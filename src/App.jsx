232687cb4774188bbb012e28ebce8b8210761378
Alejandro <alejandro020215@gmail.com>
2022-08-05 11:35:29 -0400

    gitignore

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,6 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="about" element={<AboutUs />} />
-          <Route path="services" element={<Services />} />
           <Route path="contact" element={<ContactUs />} />
           <Route path="help" element={<Help />} />
           <Route path="login" element={<Login />} />
