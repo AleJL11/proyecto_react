2fcd036de1da0f3ebd295b8ef82e62150bf86997
jersmellox7 <jerebautista2001@gmail.com>
2022-08-09 13:46:08 -0400

    Update AboutUs.jsx

--- src/components/pages/AboutUs.jsx
+++ src/components/pages/AboutUs.jsx
@@ -1,11 +1,12 @@
-import { Link } from "react-router-dom";
-import ReCAPTCHA from "react-google-recaptcha";
+import { Router, Link, Route, Routes } from "react-router-dom";
+import React from "react";
 import Homborinfo from "/homebor_info.png";
 import Homestay_p from "/homestay_p.jpg";
-
 import User from "/icons/user.svg";
 import Mail from "/icons/mail.svg";
 import Edit from "/icons/edit.svg";
+
+
 
 
 const AboutUs = () => {
@@ -19,16 +20,37 @@ const AboutUs = () => {
         <img src={Homborinfo} alt="Imagen info" className="img-info" />
 
         <div className="container_text">
-          <p className="texto_info">Homebor is a new accommodation platform. We help schools that runs a homestay department to stay connected with their homestay families in order to bring efficiency and maximize a good experience for everyone involved.</p>
-          <p className="texto_info">The management and organization of students and homestays in a single space has been systematized. We offer a cross-cutting workflow between the different stakeholders. Consequently, generating a detailed and constant follow-up between the homestay, student and educational organization.</p>
-          <p className="texto_info">We offer a new and easy experience to find family homes in the locations closest to your academy with updated and intuitive tools for users. Register and become part of our community.</p>
+          <p className="texto_info">
+            Homebor is a new accommodation platform. We help schools that runs a
+            homestay department to stay connected with their homestay families
+            in order to bring efficiency and maximize a good experience for
+            everyone involved.
+          </p>
+          <p className="texto_info">
+            The management and organization of students and homestays in a
+            single space has been systematized. We offer a cross-cutting
+            workflow between the different stakeholders. Consequently,
+            generating a detailed and constant follow-up between the homestay,
+            student and educational organization.
+          </p>
+          <p className="texto_info">
+            We offer a new and easy experience to find family homes in the
+            locations closest to your academy with updated and intuitive tools
+            for users. Register and become part of our community.
+          </p>
         </div>
 
         <div className="container_info2">
           <h4 className="subtitle2">Homestay</h4>
           <img src={Homestay_p} alt="Imagen info" className="img-info2" />
-          <p className="texto_info2">A Homestay is a host family that offers home accommodation services to foreign students, such as accommodation and comfortable meals. Most importantly, they facilitate a unique and meaningful cultural immersion, allowing students to better understand and experience the host country, which cannot be learned in a classroom.</p>
-          <Link className="button__info" to="/Sendemail">
+          <p className="texto_info2">
+            A Homestay is a host family that offers home accommodation services
+            to foreign students, such as accommodation and comfortable meals.
+            Most importantly, they facilitate a unique and meaningful cultural
+            immersion, allowing students to better understand and experience the
+            host country, which cannot be learned in a classroom.
+          </p>
+          <Link className="button__info" to="../Sendemail" element="Sendemail">
             Become a Homestay
           </Link>
         </div>
@@ -37,28 +59,32 @@ const AboutUs = () => {
 
           <div className="formulario">
             <label for="nombre" className="colocar_nombre">
-              First Name             
+              First Name
               <span className="obligatorio"></span>
               <img className="icon" src={User} alt="icono de usuario" />
             </label>
-            
-            <input className="campo"
-            
+
+            <input
+              className="campo"
               type="text"
               name="introducir_nombre"
               id="nombre"
               required="obligatorio"
               placeholder="e.g. Jhon"
             />
+            <small className="form-control-error">
+              Without special characters or numbers
+            </small>
           </div>
 
           <div className="formulario2">
             <label for="email" className="colocar_email">
               Email
               <img className="icon" src={Mail} alt="icono de usuario" />
-              <span class="obligatorio"></span>
+              <span className="obligatorio"></span>
             </label>
-            <input className="campo"
+            <input
+              className="campo"
               type="email"
               name="introducir_email"
               id="email"
@@ -71,13 +97,14 @@ const AboutUs = () => {
             <label for="nombre" className="colocar_texto">
               Subject
               <img className="icon" src={Edit} alt="icono de usuario" />
-              <span class="obligatorio"></span>
+              <span className="obligatorio"></span>
             </label>
-            <input className="campo"
+            <input
+              className="campo"
               type="text"
               name="introducir_nombre"
               id="nombre"
-              required="obligatorio"
+              required=""
               placeholder="e.g. How can i Register"
             />
           </div>
@@ -85,28 +112,29 @@ const AboutUs = () => {
           <div className="formulario4">
             <label for="mensaje" className="colocar_mensaje">
               Message Content
-              <span class="obligatorio"></span>
+              <span className="obligatorio"></span>
             </label>
-            <textarea 
+            <textarea
               name="introducir_mensaje"
               className="campo1"
               id="mensaje"
               required="obligatorio"
               placeholder="e.g. How can i Register"
             ></textarea>
-          </div>
 
-          <Link className="button__info2" to="/">
-            Send Message
-          </Link>
-        </div>
+          </div>       
 
-        <div className="container_info3">
-        
+            
+                <Link className="button__info2"  to="/src/components/Sendemail.jsx">          
+                  Send Message
+                </Link>
+  
+          
+      
+              </div>
 
-        </div>
-      </div>
-
+              <div className="container_info3"></div>
+            </div>
       
     </>
   );
