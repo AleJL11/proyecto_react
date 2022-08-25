4811a8b4a5f03849c392d5393257e8c2a909b114
OrianH <99349038+OrianH@users.noreply.github.com>
2022-08-25 09:11:33 -0400

    Add files via upload
    No se si se esta subiendo esta vaina

---
+++ Help.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Mmenu from "../help_partes/Mmenu";
+import Questions from "../help_partes/Questions";
+import Fpage from "../help_partes/Fpage";
+
+const Help = () => {
+ return (
+     <> 
+        <Mmenu/>
+
+        <Questions/>
+
+        <Fpage/>
+     </>
+
+ );
+};
+
+export default Help;
+
