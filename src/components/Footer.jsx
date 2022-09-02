f8544082a8c8786bb5ceccaf0cd370addb8a21f9
jersmellox7 <jerebautista2001@gmail.com>
2022-09-02 09:20:45 -0400

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFacebook, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons';
 import { Link } from 'react-router-dom';
 
-const Footer = () => {
+export const Footer = () => {
     return (
         <div className='container__footer'>
             <div className="img_footer">
@@ -13,13 +13,13 @@ const Footer = () => {
             <div className="ts-copyright">(C) Copyright 2021, All rights reserved
                 <div className="ts-footer-nav">
                     <nav className="nav">
-                        <a href="https://www.facebook.com/homebor.platform" target="_blank" class="nav-link">
+                        <a Link="https://www.facebook.com/homebor.platform" target="_blank" class="nav-link">
                             <FontAwesomeIcon icon={faFacebook} />
                         </a>
-                        <a href="https://www.twitter.com/@Homebor_Platfrm" target="_blank" class="nav-link">
+                        <a Link="https://www.twitter.com/@Homebor_Platfrm" target="_blank" class="nav-link">
                             <FontAwesomeIcon icon={faTwitter} />
                         </a>
-                        <a href="https://instagram.com/homebor.platform?utm_medium=copy_link" target="_blank" class="nav-link">
+                        <a Link="https://instagram.com/homebor.platform?utm_medium=copy_link" target="_blank" class="nav-link">
                             <FontAwesomeIcon icon={faInstagram} />
                         </a>
                     </nav>
@@ -28,4 +28,3 @@ const Footer = () => {
         </div>
     )
 }
-export default Footer;
