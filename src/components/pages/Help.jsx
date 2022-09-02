8bdef7b850c152e87afabc4f604c210acc99f620
jersmellox7 <jerebautista2001@gmail.com>
2022-09-02 09:22:16 -0400

    Update Help.jsx

--- src/components/pages/Help.jsx
+++ src/components/pages/Help.jsx
@@ -1,19 +1,21 @@
-import React from "react";
+import React from 'react';
 import Mmenu from "../help_partes/Mmenu";
 import Questions from "../help_partes/Questions";
 import Fpage from "../help_partes/Fpage";
 
 
-const Help = () => {
+export const Help = () => {
     return (
-        
-        <h4>Help Page</h4>
-    );
-    
+
+        <> 
+                <h4>Help Page</h4>
+        <Mmenu/>
+
+        <Questions/>
+
+        <Fpage/>
+     </>
+    )
 }
-
-
-
-export default Help;
 
 
