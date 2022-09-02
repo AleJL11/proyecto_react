9c227e07fa95bdc110b8da52dee7bc5c0a21542f
jersmellox7 <jerebautista2001@gmail.com>
2022-09-02 09:20:27 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
 import React from "react";
 import { Routes, Route } from 'react-router-dom';
 
-import Layout from './components/Layout';
-import Home from './components/pages/Home';
-import AboutUs from './components/pages/AboutUs';
-import ContactUs from './components/pages/ContactUs';
-import Help from './components/pages/Help';
-import Login from './components/pages/Login';
-import Register from './components/pages/Register';
-import Footer from "./components/Footer";
-import Sendemail from "./components/Sendemail";
+import { Footer, Layout, Sendemail } from './components';
+import { Home, AboutUs, ContactUs, Help, Login, Register } from './components/pages';
 
 import "./App.css";
+
+
 
 function App() {
 
@@ -22,12 +17,11 @@ function App() {
           <Route index element={<Home />} />
           <Route path="about" element={<AboutUs />} />
           <Route path="contact" element={<ContactUs />} />
-          <Route path="help" element={<Help />} /> 
+          <Route path="help" element={<Help />} />
           <Route path="login" element={<Login />} />
           <Route path="register" element={<Register />} />
         </Route>
-        <Route path="sendemail" element={<Sendemail/>}/>
-        
+        <Route path="sendmail" element={<Sendemail />} />
       </Routes>
       <Footer/>
     </>
