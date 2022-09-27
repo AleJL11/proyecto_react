4c10abed39d67b0a77053cf7ac0907d6f4ca3c69
chjersmellox7 <jerebautista2001@gmail.com>
2022-09-27 12:49:26 -0400

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,39 +1,46 @@
-import { Link } from 'react-router-dom';
-import { HashLink } from 'react-router-hash-link';
-import '../App.css';
-import HomeborLogo from '../assets/img/homebor_logo.png';
+import { Link } from "react-router-dom";
+import { HashLink } from "react-router-hash-link";
+import "../App.css";
+import HomeborLogo from "../assets/img/homebor_logo.png";
+import navmenu from "react-bootstrap/Navbar";
 
 export const Header = () => {
-    return (
-        <div className='container__header'>
-            <div className="logo">
-                <img src={HomeborLogo} alt="Homebor Logo" />
-            </div>
-            <div className="container__menu">
-                <ul className="menu">
-                    <li className="item__menu">
-                        <Link className='link' to="/">Home</Link>
-                    </li>
-                    <li className="item__menu">
-                        <Link className='link' to="about">About Us</Link>
-                                           </li>
-                    <li className="item__menu">
-                        <HashLink className='link' to="#services">Services</HashLink>
-                    </li>
-                    <li className="item__menu">
-                        <HashLink className='link' to="#contact">Contact Us</HashLink>
-                    </li>
-                    <li className="item__menu">
-                        <Link className='link' to="help">Help</Link>
-                    </li>
-                    <li className="item__menu blue">
-                        <Link className='link white' to="login">Login</Link>
-                    </li>
-                    <li className="item__menu purple">
-                        <Link className='link white' to="register">Register</Link>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    )
+  return (
+    <div className="container">
+      <div className="container">
+        <img src={HomeborLogo} alt="Homebor Logo" />
+      </div>
+      <div className="container__nav">
+        <ul className="nav justify-content-center">
+          <li className="nav-item">
+            <Link className="nav-link active" aria-current="page" to="/">
+              Home
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="about">
+              AboutUs
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="#services">
+              Services
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="contact">
+              ContactUs
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="help">
+              Help
+            </Link>
+          </li>
+          <button class="btn btn-primary" type="submit">Login</button>
+          <button class="btn btn-primary" type="submit">Register</button>
+        </ul>
+      </div>
+    </div>
+  );
 };
