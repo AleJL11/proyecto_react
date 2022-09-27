ef9c9c81adf52362848e82ece30b6a97c1a4ee31
chjersmellox7 <jerebautista2001@gmail.com>
2022-09-27 12:49:02 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Routes, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 import { Footer, Layout, Sendemail } from './components/';
